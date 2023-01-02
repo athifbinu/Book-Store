8c1d7646edc18fab71c7d765d3b846692b66c020
Athif binu <binuathif@gmail.com>
2023-01-03 00:02:46 +0530

    add state

--- frontent/src/components/Header.js
+++ frontent/src/components/Header.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import {AppBar, Typography} from '@mui/material'
-import BookIcon from '@mui/icons-material/Book';
+import {AppBar, Tab, Toolbar, Typography,Tabs} from '@mui/material'
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import { useState } from 'react';
+import {NavLink} from 'react-router-dom'
 const Header=() =>{
+      
+    const [value, setValue] = useState(0);
+
   return (
     <div>
-        <AppBar position='sticky'>
-             <Typography><BookIcon/></Typography>
-        </AppBar>
-
-    </div>
+    <AppBar sx={{backgroundColor: "#232F3D"}} position='sticky'>
+        <Toolbar>
+        <Typography><LibraryBooksIcon/></Typography>
+        <Tabs sx={{ml:'auto'}} textColor='inherit' indicatorColor='primary' value={value} onChange={(e,val) => setValue(val)}>
+        <Tab to='/' style={{color:'white'}} LinkComponent={NavLink} label='Home' />
+            <Tab LinkComponent={NavLink} to="/add" label='Add Book'/>
+            <Tab LinkComponent={NavLink} to="/books" label="Books"/>
+            <Tab LinkComponent={NavLink} to="/about" label='About Us'/>
+        </Tabs>
+        </Toolbar>
+        
+    </AppBar>
+</div>
   )
 }
 
