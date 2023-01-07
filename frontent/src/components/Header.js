32ec9dbc3cc7055e47e872f4c91707b0eb7a3708
Athif binu <binuathif@gmail.com>
2023-01-07 19:50:36 +0530

    err solve

--- frontent/src/components/Header.js
+++ frontent/src/components/Header.js
@@ -2,25 +2,32 @@ import React from 'react'
 import {AppBar, Tab, Toolbar, Typography,Tabs} from '@mui/material'
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import { useState } from 'react';
-import {NavLink} from 'react-router-dom'
+
+
 const Header=() =>{
       
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState();
 
   return (
     <div>
-    <AppBar sx={{backgroundColor: "#232F3D"}} position='sticky'>
-        <Toolbar>
-        <Typography><LibraryBooksIcon/></Typography>
-        <Tabs sx={{ml:'auto'}} textColor='inherit' indicatorColor='primary' value={value} onChange={(e,val) => setValue(val)}>
-        <Tab to='/' style={{color:'white'}} LinkComponent={NavLink} label='Home' />
-            <Tab LinkComponent={NavLink} to="/add" label='Add Book'/>
-            <Tab LinkComponent={NavLink} to="/books" label="Books"/>
-            <Tab LinkComponent={NavLink} to="/about" label='About Us'/>
-        </Tabs>
-        </Toolbar>
-        
-    </AppBar>
+      <AppBar sx={{backgroundColor:'#232F3D'}} position='sticky'>
+         <Toolbar>
+         <Typography>
+          <LibraryBooksIcon/>
+          </Typography>
+              
+          <Tabs
+            sx={{ml:'auto'}}
+           textColor='inherit' indicatorColor="secondary" value={1}
+           onChange={(e,val)=>setValue(val)}
+            >
+              <Tab  to='/add'  label='add product'/>
+              <Tab label='Books'/>
+              <Tab label='About us'/>
+          </Tabs>
+         </Toolbar>
+      </AppBar>
+    <h1>hello</h1>
 </div>
   )
 }
